36c55d65fbaecb103b69008422b32ed547d19a76
David Andersson <lejonmanen@gmail.com>
2017-01-15 18:22:24 +0100

    Added details to ITravel

---
+++ TDD_examples_1/IPrimes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1
+{
+    public interface IPrimes
+    {
+        // Returns true if n is a prime number: 2, 3, 5, 7, 11, ...
+        bool IsPrime(int n);
+    }
+}
--- TDD_examples_1/ITravel.cs
+++ TDD_examples_1/ITravel.cs
@@ -8,9 +8,30 @@ namespace TDD_examples_1
 {
     public interface ITravel
     {
+        /* Attempts to book a trip (buy a ticket) to the destination.
+         * The parameter must be a non-empty string that is not equal
+         * to the origin position. Returns true if successful.
+         */
         bool BookTrip(string destination);
+
+        /* Attempts to define the user's current location. The
+         * parameter must be a non-empty string that is not equal to
+         * the destination position. Returns true if successful.
+         */
         bool SetOrigin(string origin);
+
+        /* Attempts to travel from an origin to a destination.
+         * Requires both origin and destination to be set. Should
+         * throw an exception if either origin or destination has not
+         * been set. Returns false if origin and destination have been
+         * set, but have incorrect values; otherwise returns true.
+         */
         bool GoOnTrip();
+
+        /* Returns the current position of the user. Before a trip
+         * has been made, the position is equal to origin. After a
+         * trip it is equal to destination. May throw an exception.
+         */
         string GetPosition();
     }
 
