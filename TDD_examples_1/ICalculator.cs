4d4e55aaa1738cf50d8b5071b8de988f8e6ffd1e
David Andersson <lejonmanen@gmail.com>
2017-01-15 18:38:57 +0100

    partial implementation of ICalculator

---
+++ TDD_examples_1/ICalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1
+{
+    public interface ICalculator
+    {
+        void EnterValue(double x);
+        void Add();
+        void Subtract();
+        void Multiply();
+        double Result();
+    }
+}
---
+++ TDD_examples_1/implementations/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1.implementations
+{
+    public class Calculator : ICalculator
+    {
+        // I don't think this is completely bug free...
+        // maybe the tests will find the bugs?
+
+        private double value = double.NaN, result = double.NaN;
+
+        public void Add()
+        {
+            result += value;
+            result = double.NaN;
+        }
+
+        public void EnterValue(double x)
+        {
+            if (result == double.NaN)
+            {
+                result = x;
+                value = double.NaN;
+            }
+            else
+                value = x;
+        }
+
+        public void Multiply()
+        {
+            result *= value;
+            value = double.NaN;
+        }
+
+        public double Result()
+        {
+            return result;
+        }
+
+        public void Subtract()
+        {
+            result *= value;
+            value = double.NaN;
+        }
+    }
+}
