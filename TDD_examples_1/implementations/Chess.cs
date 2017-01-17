97f36e7d4474ce436b451aad413864cf9f6b201b
David Andersson <lejonmanen@gmail.com>
2017-01-17 22:59:08 +0100

    Chess file added

---
+++ TDD_examples_1/implementations/Chess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1.implementations
+{
+    public class Chess : IChessGame
+    {
+        // TODO: needs some data type to hold the 8x8 board
+
+        public Piece getPieceAt(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void movePiece(int rowFrom, int columnFrom, int rowTo, int columnTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void putPiece(Piece p, int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setupBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
