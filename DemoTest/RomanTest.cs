9f4722f7923343502f7a9f244ca92768d2e50b47
David <lejonmanen@gmail.com>
2017-01-18 13:02:51 +0100

    roman number test cases added

---
+++ DemoTest/RomanTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_examples_1.implementations;
+
+namespace DemoTest
+{
+    /// <summary>
+    /// Summary description for RomanTest
+    /// </summary>
+    [TestClass]
+    public class RomanTest
+    {
+        public RomanTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void AllowedNumberIsCorrectTest()
+        {
+            Roman r = new Roman();
+            int x = 2017;
+            string expected = "MMXVII";
+            string actual = r.IntToRoman(x);
+            Assert.AreEqual(expected, actual,
+                "Wrong roman number for " + x);
+
+            RomanHelper(r, 3999, "MMMCMXCIX");
+            RomanHelper(r, 1, "I");
+            RomanHelper(r, 21, "XXI");
+            RomanHelper(r, 400, "CD");
+            RomanHelper(r, 444, "CDXLIV");
+            RomanHelper(r, 43, "XLIII");
+            RomanHelper(r, 443, "CDXLIII");
+        }
+        private void RomanHelper(Roman r, int x, string expected)
+        {
+            string actual = r.IntToRoman(x);
+            Assert.AreEqual(expected, actual,
+                "Wrong roman number for " + x);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DisallowedNumberThrowsException_4000()
+        {
+            Roman r = new Roman();
+            r.IntToRoman(4000);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DisallowedNumberThrowsException_0()
+        {
+            Roman r = new Roman();
+            r.IntToRoman(0);
+        }
+    }
+}
---
+++ TDD_examples_1/implementations/Roman.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1.implementations
+{
+    public class Roman : IRoman
+    {
+        public string IntToRoman(int number)
+        {
+            if (number > 3999 || number < 1)
+                throw new ArgumentOutOfRangeException();
+
+            // IVXLCDM
+            string result = "";
+            while (number > 0)
+            {
+                if (number >= 1000)
+                {
+                    result += "M";
+                    number -= 1000;
+                }
+                else if (number >= 900)
+                {
+                    result += "CM";
+                    number -= 900;
+                }
+                else if (number >= 500)
+                {
+                    result += "D";
+                    number -= 500;
+                }
+                else if (number >= 400)
+                {
+                    result += "CD";
+                    number -= 400;
+                }
+                else if (number >= 100)
+                {
+                    result += "C";
+                    number -= 100;
+                }
+                else if (number >= 90)
+                {
+                    result += "XC";
+                    number -= 90;
+                }
+                else if (number >= 50)
+                {
+                    result += "L";
+                    number -= 50;
+                }
+                else if (number >= 40)
+                {
+                    result += "XL";
+                    number -= 40;
+                }
+                else if (number >= 10)
+                {
+                    result += "X";
+                    number -= 10;
+                }
+                else if (number >= 9)
+                {
+                    result += "IX";
+                    number -= 9;
+                }
+                else if (number >= 5)
+                {
+                    result += "V";
+                    number -= 5;
+                }
+                else if (number >= 4)
+                {
+                    result += "IV";
+                    number -= 4;
+                }
+                else if (number >= 1)
+                {
+                    result += "I";
+                    number -= 1;
+                }
+            }
+            return result;
+        }
+
+        public int RomanToInt(string number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
