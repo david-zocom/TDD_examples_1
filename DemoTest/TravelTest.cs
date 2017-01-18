d479f7a48cbe762e8a64031a23337fabcab66a66
David <lejonmanen@gmail.com>
2017-01-18 15:52:19 +0100

    ITravel implementation and 90% test cases

---
+++ DemoTest/TravelTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_examples_1.implementations;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class TravelTest
+    {
+        private Travel t;
+
+        public TravelTest()
+        {
+            t = new Travel();
+
+        }
+
+        [TestMethod]
+        public void BookTrip_Success()
+        {
+            string destination = "island";
+            bool result = t.BookTrip(destination);
+            Assert.IsTrue(result, "Booking should succeed");
+        }
+        [TestMethod]
+        public void BookTrip_Fail_IllegalParameter()
+        {
+            string destination = null;
+            bool result = t.BookTrip(destination);
+            Assert.IsFalse(result, "Booking to null should fail");
+
+            destination = "";
+            result = t.BookTrip(destination);
+            Assert.IsFalse(result, "Booking to empty string should fail");
+        }
+        [TestMethod]
+        public void BookTrip_Fail_SameOriginAndDestination()
+        {
+            string destination = "Göteborg";
+            t.SetOrigin(destination);
+            bool result = t.BookTrip(destination);
+            Assert.IsFalse(result,
+                "Booking trip between same origin/destination should fail");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void GetPosition_Fail_OriginNotSet()
+        {
+            t.GetPosition();
+        }
+
+        [TestMethod]
+        public void GetPosition_Success()
+        {
+            string o = "Göteborg";
+            t.SetOrigin(o);
+            string d = t.GetPosition();
+            Assert.AreEqual(o, d, "GetPosition wrong position");
+        }
+
+        [TestMethod]
+        public void GoOnTrip_Success()
+        {
+            string o = "Göteborg", d = "Island";
+            t.SetOrigin(o);
+            t.BookTrip(d);
+            bool result = t.GoOnTrip();
+            Assert.IsTrue(result, "Trip should succeed");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void GoOnTrip_Fail_EitherNotSet()
+        {
+            t.SetOrigin("Ankeborg");
+            t.GoOnTrip();
+        }
+
+
+    }
+}
---
+++ TDD_examples_1/implementations/Travel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1.implementations
+{
+    public class Travel : ITravel
+    {
+        private string destination = null;
+        private string origin = null;
+
+        public bool BookTrip(string destination)
+        {
+            if (destination == null || destination == "")
+                return false;  // illegal value fail
+            else if (destination == origin)
+                return false;  // disallowed value fail
+
+            this.destination = destination;
+            return true;
+        }
+        public bool SetOrigin(string origin)
+        {
+            if (origin == null || origin == "")
+                return false;
+            else if (origin == destination)
+                return false;
+
+            this.origin = origin;
+            return true;
+        }
+        public string GetPosition()
+        {
+            if (origin == null || origin == "")
+                throw new Exception();
+            return origin;
+        }
+
+        public bool GoOnTrip()
+        {
+            //string.IsNullOrEmpty
+            if (origin == null || destination == null || origin == "" || destination == "")
+                throw new Exception();
+            if (origin == destination)
+                return false;
+            origin = destination;
+            destination = null;
+            return true;
+        }
+
+
+    }
+}
