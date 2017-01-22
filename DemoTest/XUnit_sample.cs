7065497b24d287a47e2a863fdf513087212bf079
David Andersson <lejonmanen@gmail.com>
2017-01-22 18:29:55 +0100

    xUnit sample

---
+++ DemoTest/XUnit_sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace DemoTest
+{
+    public class XUnit_sample
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(1 + 1 == 2, "Basic arithmetic should succeed");
+            Assert.Equal("hej", "HEJ", true);
+        }
+    }
+}
