9bdd93265b64e416933919b5f572bd26138adee7
David <lejonmanen@gmail.com>
2017-01-23 11:21:05 +0100

    FakeDatabase implementation

---
+++ DemoTest/DatabaseTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_examples_1.implementations;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void CountUsers_CorrectNumberOfUsers()
+        {
+            FakeDatabase db = new FakeDatabase();
+            NeedsTesting nt = new NeedsTesting(db);
+
+            int actual = nt.CountUsers();
+            Assert.AreEqual(0, actual,
+                "Wrong number of users, should be 0, is "+actual);
+            Assert.AreEqual(1, db.NumberOfCalls, "Wrong number of calls");
+
+            db.Users.Add(new User());
+            db.Users.Add(new User());
+            db.Users.Add(new User());
+            actual = nt.CountUsers();
+            Assert.AreEqual(3, actual,
+                "Wrong number of users, should be 3, is " + actual);
+            Assert.AreEqual(2, db.NumberOfCalls, "Wrong number of calls");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CountUsers_ExceptionIfDbNull()
+        {
+            NeedsTesting nt = new NeedsTesting(null);
+            nt.CountUsers();
+        }
+    }
+
+    public class FakeDatabase : IDatabase
+    {
+        public List<User> Users { get; set; }
+        public int NumberOfCalls { get; set; }
+
+        public FakeDatabase()
+        {
+            Users = new List<User>();
+            NumberOfCalls = 0;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            NumberOfCalls++;
+            return Users;
+        }
+    }
+}
---
+++ TDD_examples_1/implementations/Database.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_examples_1.implementations
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public interface IDatabase
+    {
+        List<User> GetAllUsers();
+    }
+
+    // Do not modify this class!!!
+    public class Database : IDatabase
+    {
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Modify this class so it can be tested
+    public class NeedsTesting
+    {
+        private IDatabase db;
+        public NeedsTesting(IDatabase replacement)
+        {
+            db = replacement;
+        }
+        public void SetDatabase(IDatabase replacement)
+        {
+            db = replacement;
+        }
+
+        // To test this method, you need to
+        // use Dependency Injection.
+        public int CountUsers()
+        {
+            List<User> users = db.GetAllUsers();
+            return users.Count();
+        }
+    }
+}
